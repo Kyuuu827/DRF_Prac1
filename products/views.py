606a5e453eba8caee1ee89c4f66d343a3488ed5e
Kyuuu827 <lck0827@gmail.com>
2022-02-05 03:39:53 +0900

    ADD: PUT 메소드 추가

--- products/views.py
+++ products/views.py
@@ -7,7 +7,7 @@ from .models        import Product
 
 
 class ProductView(APIView):
-    def get(self, request, format=None):
+    def get(self, request):
         productlist = Product.objects.all()
         serializer = ProductSerializer(productlist, many=True) # many=True는 Json을 리스트형태로 받는 것
         return Response(serializer.data)
@@ -17,4 +17,14 @@ class ProductView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = 201)
+        return Response(serializer.errors, status = 400)
+
+
+class ProductDetailView(APIView):
+    def put(self, request, id):
+        productlist = Product.objects.get(id=id)
+        serializer = ProductSerializer(productlist, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status = 400)
